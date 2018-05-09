24d2f9688b017633f156edd103978ef096db7027
Brandon Wieber <bwieber@shyftanalytics.com>
2018-05-08 22:46:21 -0400

    Simple dom manipulation library added

---
+++ js/simple-dom.js
@@ -0,0 +1,35 @@
+//const $ = function $ (selector, context = document) {
+//const elements = Array.from(context.querySelectorAll(selector))
+//
+//  return {
+//    elements,
+//
+//    html (newHtml) {
+//      this.elements.forEach(element => {
+//        element.innerHTML = newHtml
+//      })
+//
+//      return this
+//    },
+//
+//    css (newCss) {
+//      this.elements.forEach(element => {
+//        Object.assign(element.style, newCss)
+//      })
+//
+//      return this
+//    },
+//
+//    on (event, handler, options) {
+//      this.elements.forEach(element => {
+//        element.addEventListener(event, handler, options)
+//      })
+//
+//      return this
+//    }
+//
+//    // etc.
+//  }
+//}
+//
+//const greetingText = document.querySelector('#greeting');
